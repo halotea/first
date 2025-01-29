9b2b7baaa9a535d7267652764dc57250c23b280d
halotea <makhan2009@proton.me>
2025-01-28 22:09:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.addEventListener("mousemove", function(event) {
-    let cursor = document.querySelector(".cursor");
-    cursor.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
+    let box = document.querySelector(".box");
+    let xAxis = (window.innerWidth / 2 - event.clientX) / 25;
+    let yAxis = (window.innerHeight / 2 - event.clientY) / 25;
+
+    box.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
